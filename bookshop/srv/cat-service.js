d78a44a910cf5ef6b809cf7dd3cdfb6b3bacd5c2
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-07-11 09:35:12 +0200

    step: consuming other services

--- bookshop/srv/cat-service.js
+++ bookshop/srv/cat-service.js
@@ -1,13 +1,37 @@
-const cds = require('@sap/cds')
-class CatalogService extends cds.ApplicationService { init() {
-  const { Books } = cds.entities('CatalogService')
+// const cds = require('@sap/cds')
+// class CatalogService extends cds.ApplicationService { init() {
+//   const { Books } = cds.entities('CatalogService')
 
-  // Register your event handlers in here, for example:
-  this.after ('each', Books, book => { 
-    if (book.stock > 111) { 
-      book.title += ` -- 11% discount!`
-    } 
-  }) 
+//   // Register your event handlers in here, for example:
+//   this.after ('each', Books, book => { 
+//     if (book.stock > 111) { 
+//       book.title += ` -- 11% discount!`
+//     } 
+//   }) 
+
+//   return super.init()
+// }}
+// module.exports = CatalogService
+
+const cds = require('@sap/cds')
+class CatalogService extends cds.ApplicationService { async init() {
+
+  const db = await cds.connect.to('db') // connect to database service
+  const { Books } = db.entities         // get reflected definitions
+
+  // Reduce stock of ordered books if available stock suffices
+  this.on ('submitOrder', async req => {
+    const {book,quantity} = req.data
+    const n = await UPDATE (Books, book)
+      .with ({ stock: {'-=': quantity }})
+      .where ({ stock: {'>=': quantity }})
+    n > 0 || req.error (409,`${quantity} exceeds stock for book #${book}`)
+  })
+
+  // Add some discount for overstocked books
+  this.after ('each','Books', book => {
+    if (book.stock > 111) book.title += ` -- 11% discount!`
+  })
 
   return super.init()
 }}
